a1ea35e13acec88f496e7efacc1be07e72c2958b
slevin <575720013@qq.com>
2019-11-29 15:05:18 +0800

    新增docs目录用于展示案例

---
+++ shell.js
@@ -0,0 +1,16 @@
+var shell = require("shelljs");
+
+//执行npm run build 命令
+if (shell.exec('npm run build').code !== 0) {
+    shell.echo('Error: Git commit failed');
+    shell.exit(1);
+}
+
+// 打包后的文件从dist目录复制到docs目录
+// docs目录必须存在，另外如果docs目录内有同名文件会直接覆盖掉
+shell.cp('-r', './dist/*', './docs');
+
+
+// shell.exec('git add .');
+// shell.exec("git commit -m 'autocommit'")
+// shell.exec('git push')
